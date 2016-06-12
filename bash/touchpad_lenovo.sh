974cd8b74580c156ef4857ee3d4ce1fd341d5ec5
Jan Christoph Uhde <Jan@UhdeJC.com>
2016-06-12 17:39:54 +0200

    clean up in touchpad script

--- bash/touchpad_lenovo.sh
+++ bash/touchpad_lenovo.sh
@@ -6,6 +6,7 @@ o_remove_spaces(){
     var="${var%"${var##*[![:space:]]}"}"   # remove trailing whitespace characters
     echo "$var"
 }
+
 o_touchpad_call_xinput(){
     local touchpad=$(xinput --list --short | grep -m1 "SynPS/2 Synaptics TouchPad" | cut -f2 | cut -d= -f2)
 
@@ -21,23 +22,6 @@ o_touchpad_call_xinput(){
         echo "tapping $2"
         o_touchpad_call_xinput "tapping" "$2"
     fi
-
-    ##elif [[ $status == "1 1" ]]; then
-    ##    xinput set-prop $touchpad "Device Enabled" 1
-    ##    xinput set-prop $touchpad "Synaptics Tap Action" 0 0 0 0 1 0 0
-    ##    #gsettings set org.gnome.settings-daemon.peripherals.touchpad touchpad-enabled true
-    ##    #gsettings set org.gnome.settings-daemon.peripherals.touchpad tap-to-click true
-
-    ##elif [[ $status == "1 0" ]]; then
-    ##    xinput set-prop $touchpad "Device Enabled" 1
-    ##    xinput set-prop $touchpad "Synaptics Tap Action" 0 0 0 0 0 0 0
-    ##    #gsettings set org.gnome.settings-daemon.peripherals.touchpad touchpad-enabled true
-    ##    #gsettings set org.gnome.settings-daemon.peripherals.touchpad tap-to-click false
-
-    ##elif [[ $status == "0 0" ]]; then
-    ##    xinput set-prop $touchpad "Device Enabled" 0
-    ##    #gsettings set org.gnome.settings-daemon.peripherals.touchpad touchpad-enabled false
-    ##fi
 }
 
 o_touchpad_server(){
@@ -62,17 +46,12 @@ o_touchpad_get_status(){
     echo "$(o_remove_spaces $enabled) $(o_remove_spaces $tapping)"
 }
 
-
- o_touchpad_client(){
+o_touchpad_client(){
     local pipe=~/.touchpad_server
-    local touchpad_enabled="touchpad and tapping enabled"  # status 1
-    local tapping_disabled="tapping disabled"              # status 2
-    local touchpad_disabled="touchpad disabled"            # status 3
     echo "client input: $@"
 
     if [[ "$1" == toggle ]]; then
         status="$(o_touchpad_get_status)"
-        echo "status in system: '$status'"
         if [[ "$status" == "1 1" ]]; then
             echo "disable tapping"
             echo "1 0" > $pipe
@@ -90,8 +69,6 @@ o_touchpad_get_status(){
             echo "1 1" > $pipe
             notify-send -t 2000 -i input-touchpad "touchpad:on tapping:on"
         fi
-        status="$(o_touchpad_get_status)"
-        echo "status in system: '$status'"
     else
         echo $@ > $pipe
     fi
